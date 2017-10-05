681fb6f0c349ffc62e37417e10427ad59285d5bb
Victor Queiroz <victorcqueirozg@gmail.com>
2017-10-05 16:34:38 -0300

    fix indentation

--- bind-vs-new-function.js
+++ bind-vs-new-function.js
@@ -1,35 +1,36 @@
 const Benchmark = require('benchmark');
 
 class NewFunctionClass {
-	constructor() {
-		this.method1 = function(){
+    constructor() {
+        this.method1 = function(){
 
-		};
-	}
+        };
+    }
 }
 
 class BoundClass {
-	constructor() {
-		this.method1 = this.method1.bind(this);
-	}
+    constructor() {
+        this.method1 = this.method1.bind(this);
+    }
 
-	method1(){
+    method1(){
 
-	}
+    }
 }
 
 const suite = new Benchmark.Suite();
 
+
 suite.add('new function', function(){
-	new NewFunctionClass();
+    new NewFunctionClass();
 })
 .add('bind method', function(){
-	new BoundClass();
+    new BoundClass();
 })
 .on('cycle', function(event) {
-	console.log(String(event.target));
+    console.log(String(event.target));
 })
 .on('complete', function() {
-	console.log('Fastest is ' + this.filter('fastest').map('name'));
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 .run({async: true});
